1f66738ad84f5b98f824d1530bfa4c8fc3640746
stevanovicm32 <stevanovic.masa32@gmail.com>
2026-02-27 14:49:23 +0100

    Tests

--- frontend/src/components/index.js
+++
@@ -1,8 +0,0 @@
-// Export svih reusable komponenti
-export { default as Button } from './Button'
-export { default as Input } from './Input'
-export { default as Card } from './Card'
-export { default as Modal } from './Modal'
-export { default as ProtectedRoute } from './ProtectedRoute'
-export { default as Avatar, getAvatarUrl, getAvatarSeedForKorisnik, PRESET_AVATAR_SEEDS } from './Avatar'
-
--- src/Infrastructure/Routes/console.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
-
-// Console routes can be added here if needed
-
