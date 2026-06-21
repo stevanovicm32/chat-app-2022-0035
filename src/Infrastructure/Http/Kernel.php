027c9252747711dc531223725d52b0c5006cb913
stevanovicm32 <stevanovic.masa32@gmail.com>
2026-06-21 16:43:22 +0200

    feat(identity): izdvojiti Identity servis sa PostgreSQL bazom

--- src/Infrastructure/Http/Kernel.php
+++ src/Infrastructure/Http/Kernel.php
@@ -59,5 +59,6 @@ class Kernel extends HttpKernel
         'signed' => \App\Infrastructure\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'internal' => \App\Infrastructure\Http\Middleware\VerifyInternalApiKey::class,
     ];
 }
